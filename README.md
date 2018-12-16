2a2500dd68a595d03c79450b18e889448eb6b61a
mantaseus <mantaseus@gmail.com>
2018-12-15 16:12:42 -0800

    First commit - basic functionality implemented

---
+++ README.md
@@ -0,0 +1,117 @@
+# Python based Linux driver for Huion Kamvas Pro devices
+
+This is a userspace python based evdev driver for Huion Kamvas Pro devices so that they can be used on Linux machines which don't have officially supported driver.
+
+I personally own a Huion Kamvas Pro 13 and I have tested this driver successfully with that device. You driver should also be usable with other Huion Kamvas devices, but they havn't been tested
+
+Feel free to open an issue if you ran into trouble or write me a message if you found this useful. 
+
+## Features
+
+This script uses a simple configuration file to give you control over how your graphics tablet interacts with your computer. You can:
+- Define bezier pressure curves to adjust the pressure sensitivity to your needs
+- Define actions for every button on your tablet, even for pen touch. Actions can defined for the following events
+    - Pen click on the screen
+    - Pen Buttons
+    - Tablet buttons
+    - Scrollbar levels
+    - Scrollbar increment
+    - Scrollbar decrement
+
+## Requirements
+
+- Packages from your distribution (Archlinux commands shown here)
+    - Python 3: This script was tested with Python 3. It might run just fine on Python 2 if you can install the required python modules
+
+        ```
+        sudo pacman -S python3
+        ```
+    - [xf86-input-evdev](https://digimend.github.io/support/howto/drivers/evdev/): This is the Digimend evdev package which adds X events relevant to graphics tablets and makes them available to other apps
+        
+        ```
+        sudo pacman -S xf86-input-input-evdev
+        ```
+    - [xdotool](https://www.semicomplete.com/projects/xdotool/): Allows you to simulate keyboard input and other mouse activities. This will be used by the script to trigger user defined actions (in ./config.yaml file) when a certain button is pressed on the graphics tablet
+        
+        ```
+        sudo pacman -S xdotool
+        ```
+- Python packages
+    - [pyusb](https://walac.github.io/pyusb/): Used for communicating with the USB device and reading the data packets from the graphics tablet
+    
+        ```
+        pip install pyusb
+        ```
+    - [python-evdev](https://github.com/gvalkov/python-evdev): Used for adding and controlling a new xinput device. This is needed to actually use the data from the graphics tablet and move the cursor, provide pressure senistivity, etc.
+        
+        ```
+        pip install evdev
+        ```
+
+# Setup
+
+- Update your `/etc/X11/xorg.conf` file (create it if it is not already there):
+
+    ```
+    Section "InputClass"
+        Identifier "evdev tablet catchall"
+        MatchIsTablet "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "evdev"
+    EndSection
+    ```
+
+- Clone this repository onto your computer
+- You might need to make sure that the DIGImend kernel driver are not loaded. Unload them using this command
+
+    ```
+    sudo rmmod hid-uclogic
+    ```
+
+## Usage
+
+- Run the driver using
+
+    ```
+    sudo python kamvas_driver.py
+    ```
+
+    - The script will output some tablet information and you should be able to control the mouse cursor using the graphics tablet and do all the things you would expect with the graphics tablet
+- If you want to output the raw X, Y position and pressure values to the screen then you can use the following commands
+
+    ```
+    sudo python kamvas_driver.py -v
+    ```
+- To print out the raw byte data from the USB use the following command
+    
+    ```
+    sudo python kamvas_driver.py -t
+    ```
+
+## Configuration
+
+You can edit the `./config.yaml` file to define your custom settings for your graphics tablet. The config file allows you to define the following:
+- Capabilies of your graphics tablet (like its resolution, pressure sensitivity, etc)
+- Pressure Curve 
+    - A simple cubic bezier curve
+    - Define the 4 points needed to describe the bezier curve
+    - Make sure that the x and y values for the points is limited between 0 and 1 inclusive
+    - The first and last point are fixed at x=0 and x=1
+- Actions that must be performed when a button is clicked
+    - The script uses `xdotool` to perform these actions so the values for these actions can be any commands accepted by `xdotool`
+        - For example: Use `key a` to effectively execute `xdotool key a` which simulates the presseing of the `a` key on your keyboard
+    - Leave the actions field empy if you don't to perform any action for that even
+        - For example: You might not want actions to be performed when you touch the pen to the screen and want it behave like a normal mouse click. But the option if available in case you do want perform an action in that case.
+
+## Troubleshooting
+
+- The cursor behaves erratically or doesn't move at all
+    - The graphics tablet sends out a packet of bytes every time you move the pen or click a button
+    - Each byte represents a certain parameter
+        - For example: For the Huion Kamvas Pro 13 the `x_pos_msb = data_packet[3]` and `x_pos_lsb = data_packet[2]`
+    - It is possible that your graphics tablet arranges its data in a different way
+    - You can use `-t` optional argument when running the script to print the raw byte packets
+        - Each column represents a piece of data being sent by the tablet 
+    - Observe how the values change when you perform certain actions
+        - For example: Try to slowly move the pen from left to right and see how the values in the packet change. The values that change are most likely related to the X postion. Of those values, the byte changes more slowly is likely to be the MSB and the one that changes faster is likely to be the LSB.
+    - Modify the code as needed and test your findings
---
+++ kamvas_driver.py
@@ -0,0 +1,248 @@
+from __future__ import print_function
+
+import usb.core
+import usb.util
+import sys
+from evdev import UInput, ecodes, AbsInfo
+import time
+import subprocess
+
+# sudo pip install PyYAML
+import yaml
+
+# CONFIG ------------------------------------------------------------------------------------------
+
+PEN_MAX_X = 58752
+PEN_MAX_Y = 33048
+PEN_MAX_Z = 8191
+PEN_MAX_TILT_X = 60
+PEN_MAX_TILT_Y = 60
+RESOLUTION = 5080
+
+PEN_CLICK_ACTION = 'key a'
+PEN_BTN1_ACTION = 'key b'
+PEN_BTN2_ACTION = 'key c'
+
+TABLET_BTN_ACTIONS = {
+    1: 'key d',
+    2: 'key e',
+    4: 'key f',
+    8: 'key g',
+    16: 'key h',
+}
+
+TABLET_SCROLLBAR_INCREASE_ACTION = 'key i'
+TABLET_SCROLLBAR_DECREASE_ACTION = 'key j'
+TABLET_SCROLLBAR_ACTIONS = {
+    1: 'key k',
+    2: 'key l',
+    3: 'key m',
+    4: 'key n',
+    5: 'key o',
+    6: 'key p',
+    7: 'key q',
+}
+
+# GLOBALS -----------------------------------------------------------------------------------------
+
+previous_scrollbar_state = 0
+previous_clicked_btn = 0
+config = {}
+
+# HELPER FUNCTIONS --------------------------------------------------------------------------------
+
+def print_array(array, spacing=5):
+    string = ''
+    for element in array:
+        string = string + str(element) + ' '*(spacing-len(str(element)))
+    print(string)
+
+def print_tablet_info(device):
+    print('TABLET INFORMATION -------------------------------------------------')
+    for bRequest in range(256):
+        try:
+            result = usb.util.get_string(dev, bRequest)
+            print('{}: {}'.format(hex(bRequest), result))
+        except:
+            pass
+    print('--------------------------------------------------------------------')
+
+def get_args():
+    args = {}
+
+    if '-v' in sys.argv:
+        args['-v'] = True
+    else:
+        args['-v'] = False
+
+    if '-t' in sys.argv:
+        args['-t'] = True
+    else:
+        args['-t'] = False
+
+    return args
+
+def run_action(action):
+    cmd="xdotool {}".format(action)
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(e)
+
+def load_config(action):
+    with open('config.yaml', 'r') as yaml_file:
+        config_load = yaml.load(yaml_file)
+    
+    if config_load.get('pen', 0):
+        config['pen'] = config_load['pen']
+    else:
+        print('The \'./config.yaml\' file does not have the \'pen\' property')
+        exit()
+
+    if config_load.get('pen', 0):
+        config['pressure_curve'] = config_load['pressure_curve']
+    else:
+        print('The \'./config.yaml\' file does not have the \'pressure_curve\' property')
+        exit()
+
+    if config_load.get(action, 0):
+        config[action] = config_load[action]
+    else:
+        print('The \'./config.yaml\' file does not have the \'{}\' property'.format(action))
+        exit()
+
+# MAIN --------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    args = get_args()
+
+    # Define the events that will be triggered by the custom xinput device that we will create
+    pen_events = {
+        # Defining a pressure sensitive pen tablet area with 2 stylus buttons and no eraser
+        ecodes.EV_KEY: [
+            ecodes.BTN_TOUCH, 
+            ecodes.BTN_TOOL_PEN, 
+            ecodes.BTN_STYLUS, 
+            ecodes.BTN_STYLUS2],
+        ecodes.EV_ABS: [
+            #AbsInfo input: value, min, max, fuzz, flat, resolution
+            (ecodes.ABS_X, AbsInfo(0,0,PEN_MAX_X,0,0,RESOLUTION)),         
+            (ecodes.ABS_Y, AbsInfo(0,0,PEN_MAX_Y,0,0,RESOLUTION)),
+            (ecodes.ABS_PRESSURE, AbsInfo(0,0,PEN_MAX_Z,0,0,0)),
+            (ecodes.ABS_TILT_X, AbsInfo(0,0,PEN_MAX_TILT_X,0,0,0)),
+            (ecodes.ABS_TILT_Y, AbsInfo(0,0,PEN_MAX_TILT_Y,0,0,0)),
+        ],
+        #ecodes.EV_MSC: [ecodes.MSC_SCAN], #not sure why, but it appears to be needed
+    }
+
+    #load_config('krita')
+    
+    # Try to get a reference to the USB we need
+    dev = usb.core.find(idVendor=0x256c, idProduct=0x006e)
+    if not dev:
+        print("could not find device. The device may alread be open", file=sys.stderr)
+        sys.exit(1)
+    
+    # Forcefully claim the interface from any other script that might have been using it
+    for cfg in dev:
+        for interface in cfg:
+            if dev.is_kernel_driver_active(interface.index):
+                dev.detach_kernel_driver(interface.index)
+                usb.util.claim_interface(dev, interface.index)
+                print("grabbed interface {}".format(interface.index))
+    
+    # The method needs to be called or otherwise the tablet may not be in the correct mode
+    # and no output might be seen from the first endpoint after a tablet reboot
+    print_tablet_info(dev)
+   
+    # Seems like we need to try and read atleast once from the second endpoint on the device
+    # or else the output from the first endpoint may get blocked on a tablet reboot 
+    try:
+        endpoint_1 = dev[0][(1,0)][0]
+        data = dev.read(endpoint_1.bEndpointAddress,endpoint_1.wMaxPacketSize)
+    except: pass
+
+    # Create a virtual pen in /dev/input/ so that it shows up as a XInput device
+    vpen = UInput(events=pen_events, name="kamvas-pen", version=0x3)
+    print('huion kamvas GT191 driver should now be running')
+    
+    # Get a reference to the end that the tablet's output will be read from 
+    endpoint_0 = dev[0][(0,0)][0]
+    
+    # Read the tablet output in an infinite loop
+    while True:
+        try:
+            # Read data from the USB
+            data = dev.read(endpoint_0.bEndpointAddress,endpoint_0.wMaxPacketSize)
+
+            # Calculate the values            
+            pen_x = (data[3] << 8) + (data[2])
+            pen_y = (data[5] << 8) + data[4]
+            pen_pressure = (data[7] << 8) + data[6]
+            pen_tilt_x = data[10] >= 128 and (data[10]-256) or data[10]
+            pen_tilt_y = data[11] >= 128 and (data[11]-256) or data[11]
+
+            # Send data to the Xinput device so that cursor responds
+            vpen.write(ecodes.EV_ABS, ecodes.ABS_X, pen_x)
+            vpen.write(ecodes.EV_ABS, ecodes.ABS_Y, pen_y)
+            vpen.write(ecodes.EV_ABS, ecodes.ABS_PRESSURE, pen_pressure)
+            vpen.write(ecodes.EV_ABS, ecodes.ABS_TILT_X, pen_tilt_x)
+            vpen.write(ecodes.EV_ABS, ecodes.ABS_TILT_Y, pen_tilt_y)
+
+            if data[1] == 129:
+                if PEN_CLICK_ACTION:
+                    run_action(PEN_CLICK_ACTION)             
+                else:
+                    vpen.write(ecodes.EV_KEY, ecodes.BTN_TOUCH, 1)
+
+            if data[1] == 130:
+                if PEN_BTN1_ACTION:
+                    run_action(PEN_BTN1_ACTION)             
+                else:
+                    vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, 1)
+
+            if data[1] == 132:
+                if PEN_BTN2_ACTION:
+                    run_action(PEN_BTN2_ACTION)             
+                else:
+                    vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, 1)
+
+            # Tablet buttons
+            if data[1] == 224:
+                btn_index = data[4]
+                if btn_index != previous_clicked_btn and TABLET_BTN_ACTIONS.get(btn_index, 0):
+                    run_action(TABLET_BTN_ACTIONS.get(btn_index,0))
+                previous_clicked_btn = btn_index
+
+            # Scrollbar
+            if data[1] == 240:
+                scrollbar_state = data[5]
+
+                if TABLET_SCROLLBAR_INCREASE_ACTION and scrollbar_state > previous_scrollbar_state:
+                    run_action(TABLET_SCROLLBAR_INCREASE_ACTION)
+                elif TABLET_SCROLLBAR_DECREASE_ACTION and scrollbar_state < previous_scrollbar_state:
+                    run_action(TABLET_SCROLLBAR_DECREASE_ACTION)
+
+                if scrollbar_state != previous_scrollbar_state and TABLET_SCROLLBAR_ACTIONS.get(scrollbar_state,0):
+                    run_action(TABLET_SCROLLBAR_ACTIONS.get(scrollbar_state, 0))
+
+                previous_scrollbar_state = scrollbar_state
+            
+            #vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, pen_btn1_clicked and 1 or 0)
+            #vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, pen_btn2_clicked and 1 or 0)
+            vpen.syn()
+            
+            if args['-t']:
+                print_array(data, 8)
+    
+            if args['-v']:
+                print("X {} Y {} PRESS {}          ".format(
+                    pen_x, 
+                    pen_y, 
+                    pen_pressure, 
+                ), end='\r')
+        except usb.core.USBError as e:
+            data = None
+            if e.args == ('Operation timed out',):
+                print(e, file=sys.stderr)
+        
