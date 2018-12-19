3ff5614430d76175e98833982df998cf6d3d0f1e
mantaseus <mantaseus@gmail.com>
2018-12-18 21:53:45 -0800

    now performs actions using evdev which is much faster

--- kamvas_driver.py
+++ kamvas_driver.py
@@ -7,13 +7,18 @@ import sys
 from evdev import UInput, ecodes, AbsInfo
 import time
 import subprocess
+import math
 
 import json
 
 # GLOBALS -----------------------------------------------------------------------------------------
 
+ACTION_SPLIT_CHAR = '+'
+
 previous_scrollbar_state = 0
-previous_clicked_btn = 0
+previous_tablet_btn = 0
+previous_pen_state = 0
+previous_action = ''
 config = {}
 
 # HELPER FUNCTIONS --------------------------------------------------------------------------------
@@ -49,40 +54,97 @@ def get_args():
 
     return args
 
-def run_action(action):
-    cmd="xdotool {}".format(action)
-    try:
-        subprocess.run(cmd, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        print(e)
+def run_action(new_action):
+    def execute(action_text, press_type):
+        if not action_text: return
+            
+        if ACTION_SPLIT_CHAR in action_text:
+            actions = action_text.split(ACTION_SPLIT_CHAR)
+            for action in actions:
+                action_code = ecodes.ecodes.get(action, -1)
+                if action_code == -1: return
+                vpen.write(ecodes.EV_KEY, action_code, press_type)
+        else:
+            action_code = ecodes.ecodes.get(action_text, -1)
+            if action_code == -1: return
+            vpen.write(ecodes.EV_KEY, action_code, press_type)
+  
+    global previous_action
+ 
+    if new_action == previous_action: 
+        # "hold" the previously pressed action
+        execute(previous_action, 2)
+        return
+    else:
+        # Press "up" any previously pressed action
+        execute(previous_action, 0)
+
+    if new_action:
+        # Press "down" the new action
+        execute(new_action, 1)
+
+    previous_action = new_action
 
 def load_config(action):
     with open('config.json', 'r') as json_file:
         config_load = json.load(json_file)
-   
+    pprint(config_load)
+    #import pdb; pdb.set_trace()
+    # Get the pen config
     try: 
         config['pen'] = config_load['pen']
     except:
         print('The \'./config.yaml\' file does not have the \'pen\' property')
         exit()
 
+    # Get the pressure curve
     try:
         config['pressure_curve'] = config_load['pressure_curve']
     except:
         print('The \'./config.yaml\' file does not have the \'pressure_curve\' property')
         exit()
 
+    # Get the actions config
     try:
         config['actions'] = config_load['actions'][action]
-
-        # JSON doesn't allow numeric keys for dictionaries, so we will have to make that happen
-        tablet_buttons = config['actions']['tablet_buttons']
-        config['actions']['tablet_buttons'] = {}
-        for data in tablet_buttons:
-            config['actions']['tablet_buttons'][data['id']] = data['action'] 
     except:
         print('The \'./config.yaml\' file does not have the actions named \'{}\''.format(action))
         exit()
+
+def get_required_ecodes():
+    required_ecodes = [
+        'BTN_TOUCH', 
+        'BTN_TOOL_PEN', 
+        'BTN_STYLUS', 
+        'BTN_STYLUS2'
+    ]
+
+    # Get the ecodes for pen buttons
+    for value in config['actions'].values():
+        if type(value) is list:
+            for sub_value in value:
+                if sub_value:
+                    if ACTION_SPLIT_CHAR in sub_value:
+                        required_ecodes.extend(sub_value.split(ACTION_SPLIT_CHAR))
+                    else: 
+                        required_ecodes.append(sub_value)
+        else:
+            if value:
+                if ACTION_SPLIT_CHAR in value:
+                    required_ecodes.extend(value.split(ACTION_SPLIT_CHAR))
+                else: 
+                    required_ecodes.append(value)
+
+    pprint(required_ecodes)
+    return [ecodes.ecodes[required_ecode] for required_ecode in required_ecodes]
+
+def is_list_within_list(list_var):
+    try:
+        if type(list_var[0]) is list:
+            return True
+    except:
+        return False
+    return False
 
 # MAIN --------------------------------------------------------------------------------------------
 
@@ -91,15 +153,16 @@ if __name__ == '__main__':
 
     load_config('krita')
     pprint(config)
-    
+   
     # Define the events that will be triggered by the custom xinput device that we will create
     pen_events = {
         # Defining a pressure sensitive pen tablet area with 2 stylus buttons and no eraser
-        ecodes.EV_KEY: [
-            ecodes.BTN_TOUCH, 
-            ecodes.BTN_TOOL_PEN, 
-            ecodes.BTN_STYLUS, 
-            ecodes.BTN_STYLUS2],
+        ecodes.EV_KEY: get_required_ecodes(), #[
+        #    ecodes.KEY_A, 
+        #    ecodes.BTN_TOUCH, 
+        #    ecodes.BTN_TOOL_PEN, 
+        #    ecodes.BTN_STYLUS, 
+        #    ecodes.BTN_STYLUS2],
         ecodes.EV_ABS: [
             #AbsInfo input: value, min, max, fuzz, flat, resolution
             (ecodes.ABS_X, AbsInfo(0,0,config['pen']['max_x'],0,0,config['pen']['resolution'])),         
@@ -145,65 +208,79 @@ if __name__ == '__main__':
     
     # Read the tablet output in an infinite loop
     while True:
+        #time.sleep(1)
         try:
             # Read data from the USB
             data = dev.read(endpoint_0.bEndpointAddress,endpoint_0.wMaxPacketSize)
 
-            # Calculate the values            
-            pen_x = (data[3] << 8) + (data[2])
-            pen_y = (data[5] << 8) + data[4]
-            pen_pressure = (data[7] << 8) + data[6]
-            pen_tilt_x = data[10] >= 128 and (data[10]-256) or data[10]
-            pen_tilt_y = data[11] >= 128 and (data[11]-256) or data[11]
+            if data[1] in [128, 129, 130, 131, 132, 133]:
+                # Calculate the values            
+                pen_x = (data[3] << 8) + (data[2])
+                pen_y = (data[5] << 8) + data[4]
+                pen_pressure = (data[7] << 8) + data[6]
+                pen_tilt_x = data[10] >= 128 and (data[10]-256) or data[10]
+                pen_tilt_y = data[11] >= 128 and (data[11]-256) or data[11]
 
-            # Send data to the Xinput device so that cursor responds
-            vpen.write(ecodes.EV_ABS, ecodes.ABS_X, pen_x)
-            vpen.write(ecodes.EV_ABS, ecodes.ABS_Y, pen_y)
-            vpen.write(ecodes.EV_ABS, ecodes.ABS_PRESSURE, pen_pressure)
-            vpen.write(ecodes.EV_ABS, ecodes.ABS_TILT_X, pen_tilt_x)
-            vpen.write(ecodes.EV_ABS, ecodes.ABS_TILT_Y, pen_tilt_y)
+                # Send data to the Xinput device so that cursor responds
+                vpen.write(ecodes.EV_ABS, ecodes.ABS_X, pen_x)
+                vpen.write(ecodes.EV_ABS, ecodes.ABS_Y, pen_y)
+                vpen.write(ecodes.EV_ABS, ecodes.ABS_PRESSURE, pen_pressure)
+                vpen.write(ecodes.EV_ABS, ecodes.ABS_TILT_X, pen_tilt_x)
+                vpen.write(ecodes.EV_ABS, ecodes.ABS_TILT_Y, pen_tilt_y)
+
+            # Reset any actions because this code means that nothing is happening
+            if data[1] == 128:
+                run_action('')
 
             # Pen click
             if data[1] == 129:
-                if config['actions']['pen_click']:
-                    run_action(config['actions']['pen_click'])
-                else:
-                    vpen.write(ecodes.EV_KEY, ecodes.BTN_TOUCH, 1)
+                run_action(config['actions'].get('pen_touch', ''))#, ecodes.BTN_TOUCH)
 
             # Pen button 1
             if data[1] == 130:
-                if config['actions']['pen_button_1']:
-                    run_action(config['actions']['pen_button_1'])             
-                else:
-                    vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, 1)
+                run_action(config['actions'].get('pen_button_1', ''))#, ecodes.BTN_STYLUS)
+
+            # Pen button 1 with pen touch
+            if data[1] == 131:
+                run_action(config['actions'].get('pen_button_1_touch', ''))#, ecodes.BTN_STYLUS)
 
             # Pen button 2
             if data[1] == 132:
-                if config['actions']['pen_button_2']:
-                    run_action(config['actions']['pen_button_2'])             
-                else:
-                    vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, 1)
+                run_action(config['actions'].get('pen_button_2', ''))#, ecodes.BTN_STYLUS2)
+
+            # Pen button 2 with pen touch
+            if data[1] == 133:
+                run_action(config['actions'].get('pen_button_2_touch', ''))#, ecodes.BTN_STYLUS2)
 
             # Tablet buttons
             if data[1] == 224:
-                btn_index = data[4]
-                if btn_index != previous_clicked_btn and config['actions']['tablet_buttons'].get(btn_index, 0):
-                    run_action(config['actions']['tablet_buttons'][btn_index])
-                previous_clicked_btn = btn_index
+                if data[4]:
+                    btn_index = int(math.log(data[4],2))
+                    if previous_tablet_btn != data[4] and config['actions'].get('tablet_buttons', ''):
+                        run_action(config['actions']['tablet_buttons'][btn_index])
+                    previous_tablet_btn = btn_index
+                else:
+                    run_action('')
+                    previous_tablet_btn = 0
 
             # Scrollbar
             if data[1] == 240:
                 scrollbar_state = data[5]
 
-                if config['actions']['tablet_scrollbar_increase'] and scrollbar_state > previous_scrollbar_state:
-                    run_action(config['actions']['tablet_scrollbar_increase'])
-                elif config['actions']['tablet_scrollbar_decrease'] and scrollbar_state < previous_scrollbar_state:
-                    run_action(config['actions']['tablet_scrollbar_decrease'])
+                if scrollbar_state:
+                    if previous_scrollbar_state:
+                        if scrollbar_state > previous_scrollbar_state:
+                            run_action(config['actions'].get('tablet_scrollbar_increase', ''))
+                        elif scrollbar_state < previous_scrollbar_state:
+                            run_action(config['actions'].get('tablet_scrollbar_decrease', ''))
 
-                if scrollbar_state != previous_scrollbar_state and config['actions']['tablet_scrollbar'][scrollbar_state]:
-                    run_action(config['actions']['tablet_scrollbar'][scrollbar_state])
-
+                    if scrollbar_state != previous_scrollbar_state and config['actions'].get('tablet_scrollbar', ''):
+                        run_action(config['actions']['tablet_scrollbar'][scrollbar_state-1])
+                else:
+                    run_action('')
+                    
                 previous_scrollbar_state = scrollbar_state
+
             
             #vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS, pen_btn1_clicked and 1 or 0)
             #vpen.write(ecodes.EV_KEY, ecodes.BTN_STYLUS2, pen_btn2_clicked and 1 or 0)
